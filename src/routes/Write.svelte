164fe3c6a9b820958ff4a20af7ac440ab7cbe32c
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-09-30 13:17:30 +0200

    fixed syntax typo

--- src/routes/Write.svelte
+++ src/routes/Write.svelte
@@ -1,27 +1,30 @@
 <script>
+	import { onMount } from 'svelte';
   import { scenes } from "../stores";
   export let params = {};
-  let inputScene;
   let currentScene;
 
   $: currentScene = $scenes.filter(scene => scene.id == params.sceneId)[0];
 
-  function saveScene() {
-    $scenes = $scenes.map(scene =>
-      scene.id == params.sceneId
-        ? { ...scene, content: currentScene.content }
-        : scene
-    );
-  }
+  onMount(() => {
+    if(params.sceneId !== null) {
+    document.getElementById("editor").addEventListener("input", function() {
+      $scenes = $scenes.map(scene => 
+        scene.id == params.sceneId
+          ? { ...scene, content: currentScene.content }
+          : scene
+      );
+    }, false);
+    }
+	});
 </script>
 
 {#if params.sceneId == null}
   No Scene selected
 {:else}
-  <h1 on:click={saveScene}>Test save</h1>
   <div 
+  id="editor"
   contenteditable="true"
-  bind:innerHTML={currentScene.content}
-  onchange="saveScene">
+  bind:innerHTML={currentScene.content}>
   </div>
 {/if}
