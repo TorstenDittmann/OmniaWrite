4dd252fbbfbfd534de6ae104876e35eca91c16f8
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-10-16 13:53:19 +0200

    Completely migrated all functions to store.

--- src/stores.js
+++ src/stores.js
@@ -16,7 +16,7 @@ if (localStorage.getItem("intern") === null) {
     localStorage.setItem("tabs", "[]");
 }
 
-function stateState() {
+function storeState() {
     const {
         subscribe,
         update
@@ -26,14 +26,26 @@ function stateState() {
 
     return {
         subscribe,
+        /**
+         * Sets current title.
+         * @param title Title to set.
+         */
         setCurrentTitle: (title) => update(n => {
             n.currentTitle = title;
             return n;
         }),
+        /**
+         * Sets current user ID.
+         * @param user User ID.
+         */
         setCurrentUser: (user) => update(n => {
             n.currentUser = user;
             return n;
         }),
+        /**
+         * Sets currently active project.
+         * @param project Project ID.
+         */
         setCurrentProject: (project) => update(n => {
             n.currentProject = project;
             return n;
@@ -41,7 +53,7 @@ function stateState() {
     }
 }
 
-function stateProjects() {
+function storeProjects() {
     const {
         subscribe,
         update
@@ -51,6 +63,10 @@ function stateProjects() {
 
     return {
         subscribe,
+        /**
+         * Creates project.
+         * @param title Title of the new project.
+         */
         createProject: (title) => update(n => {
             return n.concat([{
                 id: (Math.floor(Math.random() * 999) + 100),
@@ -60,7 +76,7 @@ function stateProjects() {
     }
 }
 
-function stateChapters() {
+function storeChapters() {
     const {
         subscribe,
         update
@@ -70,8 +86,12 @@ function stateChapters() {
 
     return {
         subscribe,
+        /**
+         * Creates new chapter.
+         * @param project Project ID.
+         * @param title Title of the new chapter.
+         */
         createChapter: (project, title) => update(n => {
-            console.log(n);
             return n.concat([{
                 id: (Math.floor(Math.random() * 999) + 100),
                 project: project,
@@ -82,30 +102,125 @@ function stateChapters() {
                 }
             }]);
         }),
+        /**
+         * Sets chapter title.
+         * @param id ID of the chapter.
+         * @param title New title of chapter.
+         */
         setChapterTitle: (id, title) => update(n => {
             n[n.findIndex(c => c.id == id)].title = title;
             return n;
         }),
+        /**
+         * Removes chapter.
+         * @param id ID of the chapter.
+         */
         removeChapter: (id) => update(n => {
+            return n.filter(n => n.id !== id)
+        }),
+        /**
+         * Toggles sidebar state of a chapter => Open/Closed.
+         * @param id ID of the chapter.
+         */
+        toggleChapterInSidebar: (id) => update(n => {
+            let index = n.findIndex(c => c.id == id);
+            n[index].ui.open = !n[index].ui.open;
+            return n;
+        })
+    }
+}
+
+function storeScenes() {
+    const {
+        subscribe,
+        update
+    } = writable(JSON.parse(
+        localStorage.getItem("scenes") || ""
+    ));
+
+    return {
+        subscribe,
+        /**
+         * Creates new scene.
+         * @param chapter Chapter ID.
+         * @param title Title of the new chapter.
+         */
+        createScene: (chapter, title) => update(n => {
+            return n.concat([{
+                id: (Math.floor(Math.random() * 999) + 100),
+                chapter: chapter,
+                title: title,
+                order: n.length,
+                content: ""
+            }]);
+        }),
+        /**
+         * Sets scene title.
+         * @param id ID of the scene.
+         * @param title New title of scene.
+         */
+        setSceneTitle: (id, title) => update(n => {
+            n[n.findIndex(c => c.id == id)].title = title;
+            return n;
+        }),
+        /**
+         * Sets scene content.
+         * @param id ID of the scene.
+         * @param title New content of scene.
+         */
+        setSceneContent: (id, content) => update(n => {
+            n[n.findIndex(c => c.id == id)].content = content;
+            return n;
+        }),
+        /**
+         * Removes scene.
+         * @param id ID of the scene.
+         */
+        removeScene: (id) => update(n => {
             return n.filter(n => n.id !== id)
         })
     }
 }
 
-export const scenes = writable(
-    JSON.parse(
-        localStorage.getItem("scenes") || ""
-    ));
-
-export const tabs = writable(
-    JSON.parse(
+function storeTabs() {
+    const {
+        subscribe,
+        update
+    } = writable(JSON.parse(
         localStorage.getItem("tabs") || ""
     ));
 
-export const state = stateState();
-export const projects = stateProjects();
-export const chapters = stateChapters();
+    return {
+        subscribe,
+        /**
+         * Creates new tab.
+         * @param project Project ID.
+         * @param title Title.
+         * @param link Relative link to content.
+         */
+        createTab: (project, title, link) => update(n => {
+            return n.concat([{
+                id: (Math.floor(Math.random() * 999) + 100),
+                title: title,
+                project: project,
+                link: link
+            }]);
+        }),
+        /**
+         * Removes tab.
+         * @param id ID of the tab.
+         */
+        removeTab: (id) => update(n => {
+            return n.filter(n => n.id !== id)
+        })
+    }
+}
 
+export const state = storeState();
+export const projects = storeProjects();
+export const chapters = storeChapters();
+export const scenes = storeScenes();
+export const tabs = storeTabs();
 
 projects.subscribe(val => localStorage.setItem("projects", JSON.stringify(val)));
 chapters.subscribe(val => localStorage.setItem("chapters", JSON.stringify(val)));
