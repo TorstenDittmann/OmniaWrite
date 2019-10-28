9a7e0962cd39edad9743f817ec19c5815c94ad07
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-10-28 12:36:13 +0100

    added Toolbar
    - added Toolbar
    - Prototype Notification

--- src/routes/Write.svelte
+++ src/routes/Write.svelte
@@ -32,6 +32,47 @@
       });
     }
   });
+
+  function toggleFullscreen() {
+    let element = document.documentElement;
+    if (!document.fullscreen) {
+      element.webkitRequestFullscreen();
+    } else {
+      document.webkitExitFullscreen();
+    }
+  }
+
+  function notifyMe() {
+    var text = 'HEY! Your task is now overdue.';
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification('OmniaWrite', {
+        body: text
+      });
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification('OmniaWrite', {
+            body: text
+          });
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
+  }
 </script>
 
 <style>
@@ -47,18 +88,34 @@
   }
 
   .editpane>h1 {
-    font-family: 'Roboto Mono', monospace;
+    font-family: 'Fira Mono', monospace;
     text-align: center;
   }
 
   .nodeText {
-    font-family: 'Roboto Mono', monospace;
+    font-family: 'Fira Mono', monospace;
     padding: 5%;
     color: #acbac3;
     font-weight: 400;
     font-size: 0.9rem;
     line-height: 2rem;
     text-align: justify;
+  }
+
+  .toolbar {
+    position: sticky;
+    position: -webkit-sticky;
+    top: 0;
+    text-align: center;
+    background-color: #1b2a32;
+    padding: 1rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  .redo:before {
+    display: inline-block;
+    transform: scaleX(-1);
   }
 
   @media (min-width: 1200px) {
@@ -78,7 +135,20 @@
     }
   }
 </style>
-
+<div class="toolbar">
+  <i class="icon-reply tooltip">
+    <span class="tooltiptext">Undo</span>
+  </i>
+  <i class="icon-reply redo tooltip">
+    <span class="tooltiptext">Redo</span>
+  </i>
+  <i class="icon-eye tooltip" on:click={notifyMe}>
+    <span class="tooltiptext">Focus</span>
+  </i>
+  <i class="icon-more_node_links tooltip" on:click={toggleFullscreen}>
+    <span class="tooltiptext">Fullscreen</span>
+  </i>
+</div>
 <div class="editpane">
   {#if params.sceneId == null}
     No Scene selected
