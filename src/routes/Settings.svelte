d0ee8830f947fd35ab80669cf40ed48e42bbbc1d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-10-29 13:54:57 +0100

    added prototypes
    - database
    - mindmap
    - settings

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -2,4 +2,23 @@
 
 </script>
 
-huhu::after
+<h1>Settings</h1>
+
+<h2>Account</h2>
+<div class="field">
+    <label class="big" for="editChapterInput">Connected Account:</label>
+    <input id="editChapterInput" autocomplete="off" value="test@test.de" readonly>
+</div>
+<h2>Appereance</h2>
+<div class="field">
+    <label class="big" for="editChapterInput">Theme:</label>
+    <select id="editChapterInput">
+        <option value="volvo">Dark</option>
+        <option value="saab">Light</option>
+    </select>
+</div>
+<h2>Write</h2>
+<h2>Database</h2>
+<h2>Mindmap</h2>
+<h2>Other</h2>
+<small>Alpha version</small>
