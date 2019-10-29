b954058dd013d92645809f157dafab0dd67ad21b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-10-29 13:54:57 +0100

    added prototypes
    - database
    - mindmap
    - settings

--- src/routes/Database.svelte
+++ src/routes/Database.svelte
@@ -1,3 +1,38 @@
 <script>
 
 </script>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        /* Umbrechen, sobald die Box 300 Pixel Breite hat */
+    }
+
+    .grid div {
+        background: #257989;
+        border: 2px solid #1d606d;
+        color: white;
+        margin: 1em;
+        padding: 1em;
+    }
+</style>
+
+<div class="grid">
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit facere alias, consequuntur laboriosam fugit,
+        dolor quibusdam, cumque repellat commodi dolorem nemo blanditiis molestias exercitationem obcaecati dolores
+        fugiat pariatur vero vel.</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fuga commodi minus, fugit porro consectetur
+        corporis esse officia neque nihil facere nulla architecto rerum consequuntur, ipsam ex perspiciatis beatae
+        quae.</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi fugiat praesentium unde aspernatur doloribus
+        perferendis dolore iusto nobis esse eum distinctio maxime ipsum, itaque voluptatum, sunt fuga quae libero!
+        Temporibus!</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque cum voluptates aut autem repudiandae saepe
+        quo alias, earum explicabo nobis vel quisquam, rem aperiam maxime sit porro similique. Quos, eius.</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima molestias, mollitia sapiente error natus.
+        Maiores voluptate, nesciunt magnam quos blanditiis, vero sequi enim a non consequuntur at, sit sapiente.
+        Obcaecati.</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi vel quibusdam commodi quidem quasi alias, quas
+        hic totam amet, obcaecati odit suscipit dolores iusto aliquam beatae praesentium ipsam velit maxime.</div>
+</div>
--- src/routes/Mindmap.svelte
+++ src/routes/Mindmap.svelte
@@ -1,3 +1,64 @@
 <script>
 
 </script>
+
+<style>
+    ul {
+        min-height: 100%;
+        display: inline-block;
+        vertical-align: middle;
+        list-style: none;
+    }
+
+    li {
+        display: inline-block;
+        vertical-align: middle;
+        float: left;
+        clear: left;
+        margin-top: 12px;
+        margin-bottom: 12px;
+    }
+
+    ul ul {
+        border-left: 1px solid white;
+        border-radius: 16px;
+        padding-left: 16px;
+        margin-left: 16px;
+        position: relative;
+    }
+
+    ul ul:after {
+        width: 16px;
+        height: 16px;
+        /*background-color: red;*/
+        position: absolute;
+        top: 50%;
+        content: "";
+        left: -11px;
+        margin-top: -6px;
+        width: 0;
+        height: 0;
+        border: 4px solid;
+        border-color: white black white white;
+    }
+</style>
+
+<div class="mindmap">
+    <ul contenteditable="true">
+        <li> item 1
+            <ul>
+                <li>subitem 1
+                    <ul>
+                        <li>sub-sub-item</li>
+                    </ul>
+
+                </li>
+                <li>subitem 2</li>
+                <li>subitem 3</li>
+                <li>subitem 4</li>
+                <li>subitem 5</li>
+            </ul>
+        </li>
+        <li>item 2 </li>
+    </ul>
+</div>
