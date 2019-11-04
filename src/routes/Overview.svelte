3690ce79fdf560ba017eaf14909d1247e56b64aa
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-04 13:30:46 +0100

    Update Overview.svelte
    Project changer prototype

--- src/routes/Overview.svelte
+++ src/routes/Overview.svelte
@@ -3,6 +3,24 @@
     state,
     projects
   } from "../stores";
-</script>
 
-{$state.currentProject}
+  function changeProject(project) {
+    state.setCurrentProject(project);
+    location.reload();
+  }
+
+  function setProjectTitle(project) {
+    projects.setProjectTitle(project, document.getElementById("newProjectTitle").value);
+  }
+</script>
+{#each $projects.filter(project => project.id == $state.currentProject) as project}
+<p>Opened project :{project.id}</p>
+<p>Title: <input id="newProjectTitle" type="text" value={project.title}></p>
+<button on:click={() => setProjectTitle(project.id)}>Save</button>
+<hr>
+{/each}
+<ul>
+  {#each $projects as project}
+    <li on:click={() => changeProject(project.id)}>{project.title}</li>
+  {/each}
+</ul>
