6c748e9961034aec6d10f6c25893e551ba1ec2cc
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-05 15:52:06 +0100

    added Cloud Prototype

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -1,9 +1,58 @@
 <script>
+    import {
+        onMount
+    } from "svelte";
+    import RemoteStorage from 'remotestoragejs';
+    import Widget from 'remotestorage-widget';
 
+    const remoteStorage = new RemoteStorage({
+        cache: false
+    });
+    const widget = new Widget(remoteStorage);
+
+    onMount(() => {
+        remoteStorage.setApiKeys({
+            googledrive: '554940208405-hhpi0q02gquvqrps39gptbh5r7e4qlcc.apps.googleusercontent.com'
+        });
+
+        remoteStorage.access.claim('OmniaWriteData', 'rw');
+
+        widget.attach('cloudContainer');
+    });
+
+    function saveCloud() {
+        const client = remoteStorage.scope('/OmniaWriteData/');
+
+        client.storeFile('text/plain', 'omniawrite.omniawritedata', JSON.stringify(localStorage))
+            .then(() => console.log("data has been saved"));
+    }
+
+    function getCloud() {
+        const client = remoteStorage.scope('/OmniaWriteData/');
+
+        client.getFile('omniawrite.omniawritedata').then(file => {
+            let data = JSON.parse(file.data);
+            Object.keys(data).forEach(function (k) {
+                localStorage.setItem(k, data[k]);
+            });
+        });
+    }
 </script>
+
+<style>
+    #cloudContainer #remotestorage-widget {
+        position: absolute !important;
+    }
+</style>
 
 <h1>Settings</h1>
 
+<h2>Cloud</h2>
+<button on:click={saveCloud}>Export</button>
+<button on:click={getCloud}>Import</button>
+<div id="cloudContainer">
+    <div id="remotestorage-widget"></div>
+</div>
 <h2>Account</h2>
 <div class="field">
     <label class="big" for="editChapterInput">Connected Account:</label>
