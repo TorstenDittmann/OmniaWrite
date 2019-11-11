c86809410cc157a4ba115b484dae3e917d9677bf
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-11 11:30:46 +0100

    scene re-arragnement added

---
+++ src/routes/Write/WriteOverview.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {
+        state,
+        chapters,
+        scenes
+    } from "../../stores";
+
+    $: projectData = $chapters.filter(chapter => chapter.project == $state.currentProject);
+</script>
+
+<style>
+
+</style>
+<h1>Write</h1>
