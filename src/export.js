d8bbb12a764ce23e8968906103e4d21aeb14f467
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-17 18:00:29 +0100

    added structure for exporting

--- src/export.js
+++ src/export.js
@@ -26,35 +26,47 @@ let data = {};
 
 async function fetchTemplate() {
     const start = async () => {
-        await asyncForEach(templateFiles, async (t) => {
+        asyncForEach(templateFiles, async (t) => {
             const response = await fetch(t.path);
             template[t.name] = parser.parseFromString(await response.text(), "text/xml");
         }).then(() => {
             // set project title
-            template.content.getElementById("_title").text = "Titel";
-            template.package.children[0].children[0].children[0].textContent = "Titel";
+            template.content.getElementById("_title").textContent = "Titel";
+            template.package.getElementById("_title").textContent = "Titel";
+            // set author
+            template.package.getElementById("_author").textContent = "Torsten Dittmann";
         }).then(() => {
-
-        });
+            // create chapters
+        }).then(() => {
+            // create scenes
+        }).then(() => {
+            // create files
+            zip.file("mimetype", "application/epub+zip");
+            zip.file("EPUB/package.opf", template.package.children[0].outerHTML);
+            zip.file("EPUB/nav.opf", template.nav.children[0].outerHTML);
+            zip.file("EPUB/content.opf", template.content.children[0].outerHTML);
+            zip.file("EPUB/css/epub.css", template.epub.children[0].outerHTML);
+        }).then(() => {
+            // generate zip file and save
+            zip.generateAsync({
+                type: "blob"
+            }).then(blob => {
+                saveAs.saveAs(blob, "hello-world.epub");
+            }, err => {
+                window.alert("error: " + err);
+            });
+        })
     }
     return start();
 }
 
-export function generateDownload() {
-    fetchTemplate().then(() => {
-        console.log(template)
-    });
-    /*zip.file("mimetype", "application/epub+zip");
+async function fetchData() {
 
-    zip.file("nested/hello.txt", "Hello World\n");
+}
 
-    zip.generateAsync({
-        type: "blob"
-    }).then(blob => {
-        saveAs.saveAs(blob, "hello-world.epub");
-    }, err => {
-        window.alert("error");
-    });*/
+export async function generateDownload() {
+    data = await fetchData();
+    fetchTemplate();
 }
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
