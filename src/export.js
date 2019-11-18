3c0c5f0fc2a9687cdc5fd12d63460245f8b4a836
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-18 13:58:41 +0100

    first valid export added

--- src/export.js
+++ src/export.js
@@ -1,3 +1,8 @@
+import {
+    projects,
+    chapters,
+    scenes
+} from "./stores";
 import JSZip from 'jszip';
 import saveAs from 'file-saver';
 
@@ -22,7 +27,9 @@ const templateFiles = [{
 }];
 
 let template = {};
-let data = {};
+let projectId;
+let projectImage;
+let projectData;
 
 async function fetchTemplate() {
     const start = async () => {
@@ -31,27 +38,68 @@ async function fetchTemplate() {
             template[t.name] = parser.parseFromString(await response.text(), "text/xml");
         }).then(() => {
             // set project title
-            template.content.getElementById("_title").textContent = "Titel";
-            template.package.getElementById("_title").textContent = "Titel";
+            template.content.getElementById("_title").textContent = projectData.title;
+            template.package.getElementById("_title").textContent = projectData.title;
             // set author
             template.package.getElementById("_author").textContent = "Torsten Dittmann";
         }).then(() => {
             // create chapters
+            chapters.subscribe(value => {
+                value.filter(e => e.project == projectId).sort(compare).forEach(element => {
+                    let chapterTemplate = template.content.getElementById("CHAPTER_ID").cloneNode(true);
+                    chapterTemplate.id = "chapter" + element.id;
+                    chapterTemplate.innerHTML = chapterTemplate.innerHTML.replace("CHAPTER_TITLE", element.title);
+
+                    template.content.getElementById("_body").appendChild(chapterTemplate);
+
+                    // add to nav
+                    let navTemplate = template.nav.getElementById("NAV").cloneNode(true);
+                    navTemplate.id = element.id;
+                    navTemplate.firstElementChild.href = "content.xhtml#chapter" + element.id;
+                    navTemplate.firstElementChild.textContent = element.title;
+                    template.nav.getElementById("_list").appendChild(navTemplate);
+                    scenes.subscribe(value => {
+                        value.filter(e => e.chapter == element.id).sort(compare).forEach(scene => {
+                            let sceneTemplate = template.content.getElementById("SCENE_ID").cloneNode(true);
+
+                            sceneTemplate.id = "scene" + scene.id;
+                            sceneTemplate.innerHTML = sceneTemplate.innerHTML.replace("SCENE_TITLE", scene.title);
+                            template.content.getElementById("chapter" + element.id).appendChild(sceneTemplate);
+
+                            scene.content.blocks.forEach((block, i) => {
+                                let blockTemplate = template.content.getElementById("BLOCK_ID").cloneNode(true);
+                                blockTemplate.id = "block" + i;
+                                blockTemplate.textContent = block.data.text;
+                                template.content.getElementById("scene" + scene.id).appendChild(blockTemplate);
+                            })
+                        });
+
+                    });
+                });
+            });
         }).then(() => {
-            // create scenes
-        }).then(() => {
+            const removeElements = (elms) => elms.forEach(el => el.remove());
+            removeElements(template.content.querySelectorAll("#BLOCK_ID"));
+            removeElements(template.content.querySelectorAll("#SCENE_ID"));
+            removeElements(template.content.querySelectorAll("#CHAPTER_ID"));
+            removeElements(template.nav.querySelectorAll("#NAV"));
+
             // create files
             zip.file("mimetype", "application/epub+zip");
             zip.file("EPUB/package.opf", template.package.children[0].outerHTML);
-            zip.file("EPUB/nav.opf", template.nav.children[0].outerHTML);
-            zip.file("EPUB/content.opf", template.content.children[0].outerHTML);
+            zip.file("EPUB/nav.xhtml", template.nav.children[0].outerHTML);
+            zip.file("EPUB/content.xhtml", template.content.children[0].outerHTML);
             zip.file("EPUB/css/epub.css", template.epub.children[0].outerHTML);
+            zip.file("EPUB/images/cover.png", projectImage, {
+                binary: true
+            });
+            zip.file("META-INF/container.xml", template.container.children[0].outerHTML);
         }).then(() => {
             // generate zip file and save
             zip.generateAsync({
                 type: "blob"
             }).then(blob => {
-                saveAs.saveAs(blob, "hello-world.epub");
+                saveAs.saveAs(blob, projectData.title + ".epub");
             }, err => {
                 window.alert("error: " + err);
             });
@@ -60,16 +108,28 @@ async function fetchTemplate() {
     return start();
 }
 
-async function fetchData() {
-
-}
-
-export async function generateDownload() {
-    data = await fetchData();
-    fetchTemplate();
+export async function generateDownload(id, img) {
+    projectId = id;
+    projectImage = img;
+    await projects.subscribe(value => {
+        value.filter(e => e.id == id).forEach(project => {
+            projectData = project;
+        })
+    });
+    await fetchTemplate();
 }
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
         await callback(array[index], index, array);
     }
+}
+
+function compare(a, b) {
+    if (a.order < b.order) {
+        return -1;
+    }
+    if (a.order > b.order) {
+        return 1;
+    }
+    return 0;
 }
