53ee8a793a9c292e205dc97386418ca37ae3c346
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-18 13:58:41 +0100

    first valid export added

--- src/routes/Export.svelte
+++ src/routes/Export.svelte
@@ -2,9 +2,16 @@
     import {
         generateDownload
     } from '../export';
+
+    function download(evt) {
+        const file = input.files[0];
+
+        generateDownload(241781, file)
+    }
 </script>
 <style>
 
 </style>
-test
-<button on:click={generateDownload}>Download</button>
+<input type="file" id="input">
+
+<button on:click={download}>Download</button>
