5061bae7edd8a794a6c72fed71e1edf8e9b8b02b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-19 09:50:54 +0100

    fixed serviceWorker filename

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,7 @@
 
     let updateAvailable = false;
     if ("serviceWorker" in navigator && location.hostname != "localhost") {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then(reg => {
                 reg.onupdatefound = () => {
                     const installingWorker = reg.installing;
