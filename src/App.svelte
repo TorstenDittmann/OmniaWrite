0cc6068b1a553bad6b597001bde8806c18a378f5
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-19 15:12:27 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -43,26 +43,26 @@
             scope: './'
         },
         ready(registration) {
-            console.log('Service worker is active.')
+            console.log('Service worker is active.');
         },
         registered(registration) {
-            console.log('Service worker has been registered.')
+            console.log('Service worker has been registered.');
         },
         cached(registration) {
-            console.log('Content has been cached for offline use.')
+            console.log('Content has been cached for offline use.');
         },
         updatefound(registration) {
-            console.log('New content is downloading.')
+            console.log('New content is downloading.');
         },
         updated(registration) {
-            console.log('New content is available; please refresh.')
+            console.log('New content is available; please refresh.');
             updateAvailable = true;
         },
         offline() {
-            console.log('No internet connection found. App is running in offline mode.')
+            console.log('No internet connection found. App is running in offline mode.');
         },
         error(error) {
-            console.error('Error during service worker registration:', error)
+            console.error('Error during service worker registration:', error);
         }
     });
 
