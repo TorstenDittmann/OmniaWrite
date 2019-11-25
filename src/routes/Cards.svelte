78bc5b2ab3090bf434d8d30a59e49f233967cd49
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-25 15:40:57 +0100

    added language prototype

--- src/routes/Cards.svelte
+++ src/routes/Cards.svelte
@@ -7,6 +7,9 @@
     import {
         createEventDispatcher
     } from 'svelte';
+    import {
+        _
+    } from 'svelte-i18n';
 
     let showCreateCard = false;
     let showEditCard = false;
@@ -48,52 +51,50 @@
 
 <Modal bind:show={showCreateCard}>
     <h2 slot="header">
-        New 'card'
-        <small><em>noun</em> \ ˈkärd</small>
+        {$_('cards.modal.newHeader')}
     </h2>
     <div class="field">
-        <label for="createTitle">Title:</label>
+        <label for="createTitle">{$_('cards.modal.title')}</label>
         <input id="createTitle" autocomplete="off" placeholder="enter your title" type="text"
             bind:value={newCardObject.title}>
     </div>
     <div class="field">
-        <label for="createContent">Content:</label>
+        <label for="createContent">{$_('cards.modal.content')}</label>
         <textarea id="createContent" rows="10" bind:value={newCardObject.content}></textarea>
     </div>
     <div class="field">
         <input type="checkbox" name="createTooltop" id="createTooltop" bind:checked={newCardObject.showTooltip}>
-        <label class="big" for="createTooltop">Show in scenes</label>
+        <label class="big" for="createTooltop">{$_('cards.modal.showInScenes')}</label>
     </div>
     <div class="btn-group">
-        <button on:click={createCard}>Create</button>
+        <button on:click={createCard}>{$_('cards.modal.buttonSave')}</button>
     </div>
 </Modal>
 
 <Modal bind:show={showEditCard}>
     <h2 slot="header">
-        Edit 'card'
-        <small><em>noun</em> \ ˈkärd</small>
+        {$_('cards.modal.editHeader')} '{editCardObject.title}'
     </h2>
     <div class="field">
-        <label for="createTitle">Title:</label>
+        <label for="createTitle">{$_('cards.modal.title')}</label>
         <input id="createTitle" autocomplete="off" placeholder="enter your title" type="text"
             bind:value={editCardObject.title}>
     </div>
     <div class="field">
-        <label for="createContent">Content:</label>
+        <label for="createContent">{$_('cards.modal.content')}</label>
         <textarea id="createContent" rows="10" bind:value={editCardObject.content}></textarea>
     </div>
     <div class="field">
         <input type="checkbox" name="createTooltop" id="createTooltop" bind:checked={editCardObject.showTooltip}>
-        <label class="big" for="createTooltop">Show in scenes</label>
+        <label class="big" for="createTooltop">{$_('cards.modal.showInScenes')}</label>
     </div>
     <div class="btn-group">
-        <button on:click={editCard}>Save</button>
+        <button on:click={editCard}>{$_('cards.modal.buttonSave')}</button>
     </div>
 </Modal>
 
 <div class="field">
-    <input autocomplete="off" placeholder="search here..." type="search" bind:value={searchInput}>
+    <input autocomplete="off" placeholder={$_('cards.search')} type="search" bind:value={searchInput}>
 </div>
 <div id="cards" class="grid">
     <div class="new" on:click={()=> showCreateCard = true}>
