812084016501a7889db8abe4777515723c359a67
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-26 13:36:17 +0100

    improved deskgap export

--- src/deskgap.js
+++ src/deskgap.js
@@ -1,23 +1,44 @@
 const {
     app,
     BrowserWindow,
-    messageNode
+    messageNode,
+    asyncNode,
+    dialog
 } = require("deskgap");
 
+const fs = require('fs');
+let win;
 
 app.once("ready", () => {
     messageNode.on("minimize", () => win.minimize());
     messageNode.on("maximize", () => win.maximize());
     messageNode.on("restore", () => void(0));
     messageNode.on("close", () => win.close());
+    messageNode.on("saveFile", (d, content) => {
+        dialog.showSaveDialogAsync(win, {}).then((e) => {
+            try {
+                fs.writeFileSync(e.filePath, content, 'utf-8');
+            } catch (e) {
+                alert('Failed to save the file !');
+            }
+        })
 
-    let win = new BrowserWindow({
+    })
+
+    win = new BrowserWindow({
         height: 900,
         width: 1600,
         frame: false,
         menu: null,
-        resizable: true
-    });
+        show: false
+    }).once('ready-to-show', () => {
+        win.show();
+    });;
+
+    if (process.platform !== 'win32') {
+        win.webView.setDevToolsEnabled(true);
+    }
+
     win.loadFile("public/index.html");
     win.on("closed", () => {
         win = null;
