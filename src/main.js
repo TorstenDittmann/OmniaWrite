fa175325a7a49a750ba0b8e1fe856414bca80a23
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-11-30 10:01:15 +0100

    moved language to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import App from "./App.svelte";
 
+import {
+	addMessages,
+	setInitialLocale
+} from "svelte-i18n";
+
+import en from "./lang/en.json";
+import de from "./lang/de.json";
+
+addMessages("en", en);
+addMessages("de", de);
+
+setInitialLocale({
+	fallback: "en"
+});
+
 const app = new App({
 	target: document.body
 });
