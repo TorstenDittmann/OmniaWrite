ba5a3f30d33cf8a7ea508b4cf236cec7d93f07ea
Torsten Dittmann <torsten.dittmann@gmail.com>
2019-12-09 10:07:21 +0100

    updated to 0.1.0

---
+++ src/routes/Overview/Status.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { _ } from "svelte-i18n";
+  import { deskgap } from "../../utils";
+</script>
+
+<style>
+  #cards {
+    max-width: 800px;
+  }
+  #cards div {
+    font-size: 1.5rem;
+  }
+</style>
+
+{#if !deskgap.isRunning()}
+  <div id="cards" class="grid">
+
+    <div class="new">
+      <a href="https://trello.com/b/XmcZBEIl/omniawrite" target="_blank">
+        <span class="lnr lnr-bug" />
+        {$_('developementStatus')}
+      </a>
+
+    </div>
+  </div>
+{/if}
