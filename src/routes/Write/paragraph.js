2ca23e6100961a9eacd59ef3ea3e33ae8cf7951c
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-12-13 10:38:44 +0100

    added quote tool

--- src/routes/Write/paragraph.js
+++ src/routes/Write/paragraph.js
@@ -217,3 +217,53 @@ export default class Paragraph {
     };
   }
 }
+
+export class QuoteTool {
+  static get isInline() {
+    return true;
+  }
+  constructor() {
+    this.button = null;
+    this.state = false;
+  }
+
+  render() {
+    this.button = document.createElement("button");
+    this.button.classList.add("ce-inline-tool");
+    this.button.type = "button";
+    this.button.innerHTML = "<span class=\"lnr lnr-bubble\"></span>";
+
+    return this.button;
+  }
+
+  surround(range) {
+    if (this.state) {
+      // If highlights is already applied, do nothing for now
+      return;
+    }
+
+    const selectedText = range.extractContents();
+
+    // Create MARK element
+    const mark = document.createElement("Q");
+
+    // Append to the MARK element selected TextNode
+    mark.appendChild(selectedText);
+
+    // Insert new element
+    range.insertNode(mark);
+  }
+
+
+  checkState(selection) {
+    const text = selection.anchorNode;
+
+    if (!text) {
+      return;
+    }
+
+    const anchorElement = text instanceof Element ? text : text.parentElement;
+
+    this.state = !!anchorElement.closest("Q");
+  }
+}
