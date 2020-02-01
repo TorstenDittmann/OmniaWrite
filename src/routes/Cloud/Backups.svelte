45b7ed83d78dddc6da9a7fec826d68161af4565c
Torsten Dittmann <torsten-dittmann@gmail.com>
2020-02-01 04:58:48 +0100

    styled cloud backups

--- src/routes/Cloud/Backups.svelte
+++ src/routes/Cloud/Backups.svelte
@@ -10,10 +10,58 @@
   function restoreBackup(id) {
     cloud.restoreBackup(id).then(response => deskgap.reload());
   }
+  function formatBytes(a, b) {
+    if (0 == a) return "0 Bytes";
+    var c = 1024,
+      d = b || 2,
+      e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+      f = Math.floor(Math.log(a) / Math.log(c));
+    return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f];
+  }
+
+  cloud.getAllBackups().then(res => console.log(res));
 </script>
 
 <style>
+  ul {
+    padding-inline-start: 0px;
+    margin-block-start: 0;
+    margin-block-end: 0;
+  }
+  ul li {
+    padding: 2rem 1rem;
+    display: flex;
+    justify-content: space-between;
+    opacity: 1;
+    transition: all 0.5s ease;
+    max-width: 800px;
+    cursor: pointer;
+  }
 
+  ul li:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
+  .from-now {
+    flex: 2;
+  }
+
+  .file-size {
+    flex: 1;
+  }
+
+  .date {
+    flex: 3;
+  }
+  .lnr {
+    font-size: 2rem;
+    margin-top: -0.5rem;
+  }
+
+  .file-size,
+  .date {
+    font-size: 0.8rem;
+  }
 </style>
 
 <h2>Backups</h2>
@@ -28,7 +76,14 @@
   <ul>
     {#each backups.files as backup}
       <li on:click={() => restoreBackup(backup.$uid)}>
-        {moment(backup.dateCreated, 'X').fromNow()}
+        <span class="from-now">
+          {moment(backup.dateCreated, 'X').fromNow()}
+        </span>
+        <span class="date">
+          {moment(backup.dateCreated, 'X').format('MMMM Do YYYY, h:mm:ss a')}
+        </span>
+        <span class="file-size">{formatBytes(backup.sizeOriginal)}</span>
+        <span class="lnr lnr-cloud-download" />
       </li>
     {/each}
   </ul>
