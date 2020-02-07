1d4f8f41ba11fe38ce2c32193d64b585f12f04dc
Torsten Dittmann <t.dittmann@eslgaming.com>
2020-02-07 10:18:49 +0100

    added Sentry Error Handling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/browser";
+
 import "normalize.css";
 import "./css/index.scss";
 
@@ -17,6 +19,9 @@ addMessages("de", de);
 init({
 	fallback: "en"
 });
+
+
+Sentry.init({ dsn: "https://23916d0950d744b49ded80f0177467a5@sentry.io/2319182" });
 
 const app = new App({
 	target: document.body,
