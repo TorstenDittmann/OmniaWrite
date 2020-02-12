5a72fca15f2a9033f990589d6df417bc42a0da4f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-02-13 00:01:46 +0100

    update allowRendererProcessReuse

--- src/electron.js
+++ src/electron.js
@@ -25,6 +25,7 @@ function createWindow() {
   });
 }
 
+app.allowRendererProcessReuse = true;
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
