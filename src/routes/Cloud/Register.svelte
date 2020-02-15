92a33514c804bf5bccf2f2991ae99581c2e3220d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-02-15 15:19:15 +0100

    add transitions to cloud login/register

--- src/routes/Cloud/Register.svelte
+++ src/routes/Cloud/Register.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fade } from "svelte/transition";
   import { _ } from "svelte-i18n";
   import cloud from "../../appwrite";
 
@@ -33,55 +34,57 @@
   <Policy />
 </Modal>
 
-<h2>{$_('cloud.register.title')}</h2>
-<form on:submit|preventDefault={register}>
-  <div class="field">
-    <label class="big" for="newName">{$_('cloud.register.name')}</label>
-    <input
-      id="newName"
-      type="text"
-      placeholder="John Doe"
-      autocomplete="off"
-      bind:value={registerName} />
-  </div>
-  <div class="field">
-    <label class="big" for="newUser">{$_('cloud.login.email')}</label>
-    <input
-      id="newUser"
-      type="email"
-      placeholder="john.doe@email.tld"
-      autocomplete="off"
-      bind:value={registerUser} />
-  </div>
-  <div class="field">
-    <label class="big" for="newPass">{$_('cloud.login.password')}</label>
-    <input
-      id="newPass"
-      type="password"
-      autocomplete="off"
-      placeholder="******"
-      bind:value={registerPass} />
-  </div>
-  <p>
-    <input
-      id="PrivacyPolicy"
-      type="checkbox"
-      bind:checked={statusPrivacyPolicy} />
-    <label for="PrivacyPolicy">
-      I agree to the
-      <span class="link" on:click={() => (showPrivacyPolicy = true)}>
-        {' ' + $_('cloud.privacy.show')}
-      </span>
-    </label>
-  </p>
-  <div class="btn-group">
-    <button
-      type="submit"
-      on:click={register}
-      disabled={registerButtonLoading}
-      class:loading={registerButtonLoading}>
-      <span class="lnr lnr-sync spinner" />
-      {$_('cloud.register.button')}
-    </button>
-  </div>
-</form>
+<div in:fade={{ duration: 100 }}>
+  <h2>{$_('cloud.register.title')}</h2>
+  <form on:submit|preventDefault={register}>
+    <div class="field">
+      <label class="big" for="newName">{$_('cloud.register.name')}</label>
+      <input
+        id="newName"
+        type="text"
+        placeholder="John Doe"
+        autocomplete="off"
+        bind:value={registerName} />
+    </div>
+    <div class="field">
+      <label class="big" for="newUser">{$_('cloud.login.email')}</label>
+      <input
+        id="newUser"
+        type="email"
+        placeholder="john.doe@email.tld"
+        autocomplete="off"
+        bind:value={registerUser} />
+    </div>
+    <div class="field">
+      <label class="big" for="newPass">{$_('cloud.login.password')}</label>
+      <input
+        id="newPass"
+        type="password"
+        autocomplete="off"
+        placeholder="******"
+        bind:value={registerPass} />
+    </div>
+    <p>
+      <input
+        id="PrivacyPolicy"
+        type="checkbox"
+        bind:checked={statusPrivacyPolicy} />
+      <label for="PrivacyPolicy">
+        I agree to the
+        <span class="link" on:click={() => (showPrivacyPolicy = true)}>
+          {' ' + $_('cloud.privacy.show')}
+        </span>
+      </label>
+    </p>
+    <div class="btn-group">
+      <button
+        type="submit"
+        on:click={register}
+        disabled={registerButtonLoading}
+        class:loading={registerButtonLoading}>
+        <span class="lnr lnr-sync spinner" />
+        {$_('cloud.register.button')}
+      </button>
+    </div>
+  </form>
+</div>
