561e3b4923e791907b36e54f9b07d0d0aa28ff69
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-02-24 00:11:24 +0100

    Migrate cloud from header in compoment

---
+++ src/shared/Header/Cloud.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { state, tabs } from "../../stores";
+
+  import cloud from "../../appwrite";
+  import Spinner from "../../shared/Spinner.svelte";
+
+  let isValidLogin = $state.isUserLoggedIn;
+  let showCloudUpload = false;
+  let isCloudUploading = false;
+
+  const syncCloud = () => {
+    showCloudUpload = false;
+    isCloudUploading = true;
+    cloud.saveToCloud().then(response => {
+      isCloudUploading = false;
+    });
+  };
+
+  $: {
+    if (isValidLogin) {
+      if ($state.lastCloudSave < $state.lastLocalSave) {
+        showCloudUpload = true;
+      } else {
+        showCloudUpload = false;
+      }
+    }
+  }
+</script>
+
+{#if isValidLogin}
+  {#if showCloudUpload}
+    <li on:click={syncCloud} style="-webkit-app-region: no-drag">
+      <span class="lnr lnr-cloud-upload" />
+    </li>
+  {:else}
+    <li>
+      <span class="lnr lnr-cloud-check" />
+    </li>
+  {/if}
+  {#if isCloudUploading}
+    <li>
+      <Spinner />
+    </li>
+  {/if}
+{/if}
