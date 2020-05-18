64f6b60a392b6213ced55e84ddfcdc2eee481341
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-05-19 01:05:41 +0200

    responsive fields

--- src/components/shared/Field.svelte
+++ src/components/shared/Field.svelte
@@ -1,9 +1,32 @@
 <script>
-export let id;
-export let label;
+  export let id;
+  export let label;
 </script>
+
+<style>
+  div {
+    margin: 1.5rem 0;
+  }
+  @media (min-width: 960px) {
+    .field {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      margin: 1rem 0;
+    }
+    label {
+      flex: 0 1 8rem;
+    }
+    div {
+      flex: 0 1 20rem;
+    }
+  }
+</style>
 
 <div class="field">
   <label for={id}>{label}</label>
-  <slot />
+  <div>
+    <slot />
+  </div>
 </div>
