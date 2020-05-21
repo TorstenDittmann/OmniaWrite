fc2f6e278ff22bab166982af5fc6c42089fb3b0b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-05-21 15:28:25 +0200

    add button and buttongroup component

---
+++ src/components/ButtonGroup.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<style>
+  div.button-group {
+    margin: 1rem;
+    justify-content: center;
+    display: flex;
+  }
+  div.button-group:after {
+    content: "";
+    clear: both;
+    display: table;
+  }
+</style>
+
+<div class="button-group">
+  <slot />
+</div>
