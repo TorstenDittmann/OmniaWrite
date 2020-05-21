2cddde6fbba9d865de183e67bba93b77299e73d2
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-05-21 16:51:58 +0200

    create range component

---
+++ src/components/Range.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { getRandomNumber } from "../utils";
+  import Field from "./shared/Field.svelte";
+
+  export let label;
+  export let id = label + getRandomNumber();
+
+  export let value;
+  export let min;
+  export let max;
+  export let step;
+</script>
+
+<style>
+
+</style>
+
+<Field bind:id bind:label>
+  <input
+    {id}
+    {min}
+    {max}
+    {step}
+    type="range"
+    bind:value />
+</Field>
