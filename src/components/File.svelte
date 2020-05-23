3b263b6b93aa8c5d145fd51e345844489d06a48f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-05-23 22:39:38 +0200

    add File component

---
+++ src/components/File.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { getRandomNumber } from "../utils";
+  import Field from "./shared/Field.svelte";
+
+  export let label;
+  export let id = label + getRandomNumber();
+
+  export let files;
+
+  export let required = false;
+</script>
+
+<style>
+  input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    -o-appearance: none;
+    width: 100%;
+    margin: 0;
+    padding: 0 0.25rem;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+    height: 1.2rem;
+    color: var(--text-color);
+    display: inline-block;
+    border-bottom: 1px solid #999;
+    max-height: 1.2rem;
+    font-size: 1rem;
+    opacity: 0.65;
+    background: linear-gradient(
+        to bottom,
+        transparent 95%,
+        var(--input-border-color) 95%
+      )
+      no-repeat;
+    background-size: 0 100%;
+    transition: background-size 0.2s ease;
+  }
+
+  input:focus {
+    border-bottom: 1px solid var(--input-border-color);
+    background-size: 100% 100%;
+    outline: 0;
+    opacity: 1;
+  }
+</style>
+
+<Field bind:id bind:label>
+  <input {id} {required} type="file" bind:files />
+</Field>
