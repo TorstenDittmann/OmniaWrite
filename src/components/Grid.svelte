5b9b1441ba53d6705e2db2276ca99ce884aefbed
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-06-17 23:48:30 +0200

    add Grid components

---
+++ src/components/Grid.svelte
@@ -0,0 +1,10 @@
+<style lang="scss">
+  div {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  }
+</style>
+
+<div>
+  <slot />
+</div>
---
+++ src/components/GridElement.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let title;
+  export let action = false;
+</script>
+
+<style lang="scss">
+  div {
+    background: var(--background-color);
+    border: 2px solid rgba(0, 0, 0, 0.3);
+    color: var(--text-color);
+    margin: 1em;
+    padding: 1em;
+    cursor: pointer;
+    opacity: 0.65;
+    white-space: pre-line;
+    line-height: 1.5rem;
+
+    h2 {
+      margin: 0 0 1rem 0;
+    }
+
+    &.action {
+      text-align: center;
+      font-size: 3rem;
+    }
+
+    &:hover {
+      opacity: 1;
+      transform: scale(1.05);
+      z-index: 997;
+    }
+  }
+</style>
+
+<div class:action on:click>
+  {#if title}
+    <h2>{title}</h2>
+  {/if}
+  <slot />
+</div>
