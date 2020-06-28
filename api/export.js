bea296dedadaecae85ee74d5b189f5ee80361055
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-06-28 21:12:39 +0200

    test cors settings

--- api/export.js
+++ api/export.js
@@ -1,5 +1,9 @@
 module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   const EBook = require("omnia-ebook-generator");
   const ebook = new EBook(
     {
--- api/templates.js
+++ api/templates.js
@@ -1,5 +1,9 @@
 module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   const templates = [
     {
       id: "epub3",
