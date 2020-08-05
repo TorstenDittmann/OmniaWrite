909ceffeb3df1d3562619d8f1e7056518c7e54c0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-08-05 22:54:42 +0200

    grab latest release notes for changelog

--- src/shared/NewUpdate.svelte
+++ src/shared/NewUpdate.svelte
@@ -1,24 +1,24 @@
 <script>
+  import { onMount } from "svelte";
   import { _ } from "svelte-i18n";
   import Modal from "./Modal.svelte";
+  import Spinner from "./Spinner.svelte";
 
   export let show = true;
+
+  const changelog = import(
+    /* webpackChunkName: "changelog" */ "../changelog.json"
+  );
 </script>
 
 <Modal bind:show>
   <h2 slot="header">{$_('common.update.title')}</h2>
-  <h3>Lorem Ipsum</h3>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, cumque
-    neque dolore explicabo ex quisquam voluptatum tempora, voluptatem atque
-    quidem quis quod placeat. Iusto nulla voluptate, voluptatem perferendis quos
-    consequatur?
-  </p>
-  <h3>UI improvements</h3>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, cumque
-    neque dolore explicabo ex quisquam voluptatum tempora, voluptatem atque
-    quidem quis quod placeat. Iusto nulla voluptate, voluptatem perferendis quos
-    consequatur?
-  </p>
+  {#await changelog}
+    <Spinner />
+  {:then data}
+    {#if data.changes}
+      <h3>{data.name}</h3>
+      {@html data.body}
+    {:else}{$_('common.update.noChangelog')}{/if}
+  {/await}
 </Modal>
