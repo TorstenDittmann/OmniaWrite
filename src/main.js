a0046622af9705f708b02b18c3ce2095d4b08186
Dennis Tobar <dennis.tobar@gmail.com>
2020-09-24 19:57:49 -0300

    feat(i18n): Spanish translate  (#91)
    * feat(i18n): Spanish translate
    
    I followed all instructions, but if there is a mistake, please notice me
    
    * fix(i18n): some fixes

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import "./css/index.scss";
 
 register("en", () => import(/* webpackChunkName: "en" */ "./lang/en.json"));
 register("de", () => import(/* webpackChunkName: "de" */ "./lang/de.json"));
+register("es", () => import(/* webpackChunkName: "es" */ "./lang/es.json"));
 
 init({
   fallbackLocale: "en",
