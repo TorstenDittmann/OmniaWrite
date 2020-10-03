531083ac2fd18a74cde4cad41d398d1c8eec7f59
Frédéric Bonnet <fredericbonnet@free.fr>
2020-10-03 23:20:16 +0200

    test(storybook): add stylesheet

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import { addDecorator } from "@storybook/svelte";
+import Wrapper from "./Wrapper.svelte";
+
+addDecorator(storyFn => {
+  const { Component, props, on, WrapperData } = storyFn();
+
+  return {
+    Component,
+    props,
+    on,
+    Wrapper,
+    WrapperData,
+  };
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
