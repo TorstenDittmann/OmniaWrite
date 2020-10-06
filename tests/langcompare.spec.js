a3231860f71282b25e889b97de59fec4df4555ad
Gustavo Leal <gustavohleal96@gmail.com>
2020-10-06 07:04:37 -0300

    Improving writing

--- tests/langcompare.spec.js
+++ tests/langcompare.spec.js
@@ -14,9 +14,9 @@ const parseKeys = function (obj) {
   const paths = (obj = {}, head = "") => {
     return Object.entries(obj).reduce((product, [key, value]) => {
       let fullPath = addDelimiter(head, key);
-      return isObject(value)
-        ? product.concat(paths(value, fullPath))
-        : product.concat(fullPath);
+      return isObject(value) ?
+        product.concat(paths(value, fullPath)) :
+        product.concat(fullPath);
     }, []);
   };
 
