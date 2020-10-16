79d4845220f01bb015172c7e3a5d3dc6dc92a2c0
Ole Schwagmann <syentixdev@protonmail.com>
2020-10-16 14:28:37 +0200

    Cookie is now set through Pop-Up. Doesn't ask the user until the cookie expires or is deleted.

--- src/shared/BrowserSupport.svelte
+++ src/shared/BrowserSupport.svelte
@@ -8,9 +8,26 @@
   import Modal from "./Modal.svelte";
   import Select from "../components/Forms/Select.svelte";
 
-  let show = !checkBrowser();
-  let daysIgnored;
 
+  /**
+   * @name isIgnoreSet
+   * @returns {bool} True if set, false if not
+  **/
+  const isIgnoreSet = () => {
+
+    let cookies = document.cookie.split(";")
+
+    for (let cookie of cookies) {
+      if (cookie.trim().startsWith("ignoreModal")) return true;
+    }
+
+    return false;
+  }
+
+  let show = (!checkBrowser() && !isIgnoreSet());
+
+  
+  // Options for our Select Field.
   let options = [
     {
       value: 0,
@@ -38,28 +55,44 @@
     },
   ];
 
-  const updateIgnoreCookie = () => {
+  // Variable that stores the input of the Select field
+  let daysIgnored;
+
+  /**
+   * @name setIgnoreCookie
+   * @description Sets the IgnoreModal cookie with expiration date
+  **/
+  const setIgnoreCookie = () => {
     let date = moment();
-    console.log(date);
-    console.log(daysIgnored);
+
+    if (daysIgnored == 0) {
+      document.cookie = "ignoreModal=true;expires=0;path=/";
+    } else {
+      date.add(daysIgnored, 'd')
+      document.cookie = "ignoreModal=true;expires=" + date.toDate() + ";path=/";
+    }
+
+    show = false;
   };
+
+  
 </script>
 
 <Modal bind:show>
   <h4 slot="header">Your Browser Choice</h4>
   <p>
     We respect your Browser Choice. But we kindly request for you to use the
-    desktop Apps or use the Brave Browser!
+    desktop apps or use the Brave Browser.
   </p>
   <p>
     For more information regarding browser choice and/or downloads, check out
     the <a href="https://www.omniawrite.com/get-started/">Get Started</a> page of
     our website.
   </p>
-  <form on:submit|preventDefault={updateIgnoreCookie()}>
-    <Select label="Days" bind:options bind:value={daysIgnored} />
+  <form on:submit|preventDefault={setIgnoreCookie}>
+    <Select label="Don't show this for:" bind:options bind:value={daysIgnored} />
     <ButtonGroup>
-      <Button on:click={updateIgnoreCookie}>Update</Button>
+      <Button on:click={setIgnoreCookie}>Update</Button>
     </ButtonGroup>
   </form>
 </Modal>
