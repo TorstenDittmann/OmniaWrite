7d0d84ae3bc553caf58e37d5d7b4f438d5488f9f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-02-17 20:43:47 +0100

    update to 1.3.0

--- src/shared/NewUpdate.svelte
+++ src/shared/NewUpdate.svelte
@@ -7,17 +7,6 @@
 
 <Modal bind:show>
   <h2 slot="header">{$_('common.update.title')}</h2>
-  <h3>🌐 More languages</h3>
-  <p>We added support for following languages:</p>
-  <ul>
-    <li>Simplified Chinese</li>
-  </ul>
-  <p />
-  <h3>📶 Others</h3>
-  <ul>
-    <li>New Romance E-Book template</li>
-    <li>Bug fixes</li>
-    <li>Security fixes</li>
-  </ul>
-  <p />
+  <h3>🦺 Security</h3>
+  <p>This update fixed some security issues.</p>
 </Modal>
